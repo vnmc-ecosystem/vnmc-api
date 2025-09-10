1640d6c6ab309dc771134cf2dfdf8b27bbbda1f8
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-09-10 22:32:46 +0700

    feat(path_params): extra values
    Refs: 91111182c8a8973277644822cf3fd5756e498eb5, 06a1ad6a1ea2edb49d11a57b3d39fb0523a25311, 74ddab18bfb2c3cb5a5ce9910aaa7db545d0cd82

--- src/lib/path_params.ts
+++ src/lib/path_params.ts
@@ -7,6 +7,7 @@ export const Year = t.Object({
 export const Round = t.Object({
 	round: t.UnionEnum([
 		"qual",
+		"ro64",
 		"ro32",
 		"ro16",
 		"qf",
@@ -20,6 +21,7 @@ export const Event = t.Object({
 	event: t.UnionEnum([
 		"registration",
 		"qual",
+		"ro64",
 		"ro32",
 		"ro16",
 		"qf",
@@ -30,7 +32,7 @@ export const Event = t.Object({
 });
 
 export const MapCode = t.Object({
-	code: t.RegExp("^(?:[1-5]|(?:RC[1-7]|LN[1-4]|HB[1-3]|SV[1-2])|TB)$")
+	code: t.RegExp("^(?:[1-5]|(?:RC[1-7]|LN[1-4]|HB[1-3]|SV[1-2]|EX[1-2])|TB)$")
 });
 
 export const Id = t.Object({
@@ -38,5 +40,21 @@ export const Id = t.Object({
 });
 
 export const Role = t.Object({
-	role: t.String()
+	role: t.UnionEnum([
+		"host",
+		"cohost",
+		"designer",
+		"referee",
+		"mappooler",
+		"mapper",
+		"playtester",
+		"streamer",
+		"commentator",
+		"statistician",
+		"spreadsheeter",
+		"consultant",
+		"storyboarder",
+		"map_description_writer",
+		"lore_builder"
+	])
 });
