782711dd90d7d5712a18ce9b212e891c55a388fe
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-09-30 14:04:40 +0700

    feat(lib/db): auth and init collection

---
+++ src/lib/db/auth.ts
@@ -0,0 +1,26 @@
+import { secrets } from "bun";
+import { MongoClient } from "mongodb";
+import type { Tournament, Beatmap, User, Session, Score, AvailableTimes } from "./types";
+
+const mongodb_uri = await secrets.get({
+	service: "vnmc-api",
+	name: "mongodb-uri"
+});
+
+if (!mongodb_uri) throw new Error("MongoDB URI is not set");
+
+
+
+const client = new MongoClient(mongodb_uri!);
+await client.connect();
+
+const db = client.db("vnmc");
+
+const tournaments = db.collection<Tournament>("tournaments");
+const beatmaps = db.collection<Beatmap>("beatmaps");
+const users = db.collection<User>("users");
+const sessions = db.collection<Session>("sessions");
+const scores = db.collection<Score>("scores");
+const available_times = db.collection<AvailableTimes>("available_times");
+
+export { tournaments, beatmaps, users, sessions, scores, available_times };
