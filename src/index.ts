b234dd8631db05c48143b697561a15a44a39dfba
SorA <95413012+TvSSorA@users.noreply.github.com>
2025-10-06 18:30:40 +0700

    feat: import `mappool` and `tournament` routes to main file
    Meant to do it in 61d5c85cd8995e99fe728eac26eef9646146e4cb and 68e41fc0a30bdeb235919cb0ca81ec94d07ca0a9 but i forgor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Elysia } from "elysia";
 import { map } from "./routes/map/route.ts";
+import { mappool } from "./routes/mappool/route.ts";
+import { tournament } from "./routes/tournament/route.ts";
 
 const app = new Elysia()
 	.use(map)
+	.use(mappool)
+	.use(tournament)
 	.listen(8000);
 
 console.log(
